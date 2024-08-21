acb4cf4ccea68eeab69391a085306d77042ec1e6
wms198 <wang.minshi@icloud.com>
2024-08-21 15:30:57 +0200

    Creating Another Modal(Tutorial 5)

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <h1>{{ title }}</h1>
   <p>Welcome...</p>
   <div v-if="showModal"> 
-    <Modal theme="sale1" @close="toggleModal">
+    <Modal theme="" @close="toggleModal">
       <template v-slot:links>
         <a href="#">sign up now</a>
         <a href="#">more info</a>
@@ -12,7 +12,15 @@
       <p>Grab your ninja swag for half price!</p>
     </Modal>
   </div>
+
+  <div v-if="showModalTwo"> 
+    <Modal  @close="toggleModalTwo">     
+        <h1>Sign uo to the newsletter</h1>
+        <p>For updates and promo codes!</p>
+    </Modal>
+  </div>
   <button @click.alt="toggleModal">Open modal(option/alt)</button>
+  <button @click="toggleModalTwo">Open modal</button>
   
   <!-- <input type="text" ref="name">
   <button @click="handleClick">click me</button> -->
@@ -27,14 +35,16 @@ export default {
   data() {
     return {
       title: 'My First Vue App :)',
-      header: 'Sign uo for the Giveaway!',
-      text: 'Grab your ninja swag for half price!',
-      showModal: false
+      showModal: false,
+      showModalTwo: false,
     }
   },
   methods: {
     toggleModal() {
       this.showModal = !this.showModal
+      },   
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo
       }
     }
   }
@@ -45,6 +55,7 @@ export default {
 // Custom Event: can be fired from a component and then it can be listen to from the parent component - 自定义事件：可由组件触发，然后由父组件监听
 // Click Event Modifiers(Tutorial 5)
 // Slot - 插槽(Tutorial 5): is really useful for passing custom templates into components
+// Creating Another Modal(Tutorial 5)
 </script>
 
 <style>
