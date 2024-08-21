8f691145eee32bb758523b58103b86235c13094f
wms198 <wang.minshi@icloud.com>
2024-08-21 15:41:51 +0200

    Using Teleport - 使用远程传送

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <h1>{{ title }}</h1>
   <p>Welcome...</p>
-  <div v-if="showModal"> 
+  <teleport to="#modals" v-if="showModal"> 
     <Modal theme="" @close="toggleModal">
       <template v-slot:links>
         <a href="#">sign up now</a>
@@ -11,14 +11,14 @@
       <h1>Ninja Giveaway!</h1>
       <p>Grab your ninja swag for half price!</p>
     </Modal>
-  </div>
+  </teleport>
 
-  <div v-if="showModalTwo"> 
+  <teleport to="#modals" v-if="showModalTwo"> 
     <Modal  @close="toggleModalTwo">     
         <h1>Sign uo to the newsletter</h1>
         <p>For updates and promo codes!</p>
     </Modal>
-  </div>
+  </teleport>
   <button @click.alt="toggleModal">Open modal(option/alt)</button>
   <button @click="toggleModalTwo">Open modal</button>
   
@@ -55,11 +55,11 @@ export default {
 // Custom Event: can be fired from a component and then it can be listen to from the parent component - 自定义事件：可由组件触发，然后由父组件监听
 // Click Event Modifiers(Tutorial 5)
 // Slot - 插槽(Tutorial 5): is really useful for passing custom templates into components
-// Creating Another Modal(Tutorial 5)
+// Using Teleport - 使用远程传送
 </script>
 
 <style>
-#app {
+#app, #modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
