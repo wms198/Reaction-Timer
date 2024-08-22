ad9ba1e4231cad8c53774ccfc5836a102fbb6b04
wms198 <wang.minshi@icloud.com>
2024-08-22 13:58:32 +0200

    fixed output

--- reaction-timer/src/components/Results.vue
+++ reaction-timer/src/components/Results.vue
@@ -1,13 +1,32 @@
 <template>
     <p>Reaction time - {{ score }} ms</p>
+    <p class="rank">{{ rank }}</p>
 </template>
 
 <script>
 export default {
-    props: ["score"]
+    props: ["score"],
+    data() {
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        if (this.score < 2500){
+            this.rank = "ninja Fingers"
+        } else if (this.score <4000 ){
+            this.rank = "Rapid Reflexes"
+        } else this.rank = "Snail pace..."
+    }
 }
+
+// mounted: mounted hook -> this fires as soon as the component is mounted to the dom 
 </script>
 
 <style>
-
+    .rank {
+        font-size: 1.4em;
+        color: #0faf87;
+        font-weight: bold;
+    }
 </style>
